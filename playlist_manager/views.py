6693b42b9596c1b0a24d215a1e6080a72fe22a92
Austin <mathfreak65@gmail.com>
2021-02-01 01:08:31 +0000

    Support the similar artist playlist.

--- playlist_manager/views.py
+++ playlist_manager/views.py
@@ -4,10 +4,23 @@ import requests
 import traceback
 from flask import g, jsonify, redirect, render_template, request, session, url_for
 
-from playlistmanager import createplaylist, pandora, musicbrainz
+from playlistmanager import pandora, musicbrainz
+from playlistmanager.discography_playlist import discography_playlist
+from playlistmanager.similar_artists_playlist import _get_similar_artists, similar_artists_playlist
 
 from playlist_manager.playlist_manager import app
 
+
+def _search_musicbrainz(artist_name):
+    search_result = musicbrainz.MusicBrainz.connect().search_artist(artist_name, 85)
+    choices = []
+    for artist in (search_result or []):
+        choices.append({
+            "id": artist["id"],
+            "name": artist["name"],
+            "disambiguation": artist.get("disambiguation") or artist.get("country")
+        })
+    return choices
 
 @app.errorhandler(requests.exceptions.HTTPError)
 def handle_pandora_error(err):
@@ -38,8 +51,8 @@ def show_playlists():
     elif request.method == "GET":
         return render_template("index.html")
 
-@app.route("/create", methods=["POST"])
-def create_playlist():
+@app.route("/create/discography", methods=["POST"])
+def create_discography_playlist():
     if not request.headers.get("X-PandoraAuthToken"):
         return jsonify({}), 401
 
@@ -55,28 +68,61 @@ def create_playlist():
     artist_id = request.form.get("artistId")
 
     if not artist_id:
-        search_result = musicbrainz.MusicBrainz.connect().search_artist(artist_name, 85)
+        search_result = _search_musicbrainz(artist_name)
         if not search_result:
             return jsonify({"error": f"Could not find {artist_name}"}), 404
         elif len(search_result) > 1:
-            choices = []
-            for artist in search_result:
-                choices.append({
-                    "id": artist["id"],
-                    "name": artist["name"],
-                    "disambiguation": artist.get("disambiguation") or artist.get("country")
-                })
-            return jsonify({"choices": choices, "artist": artist_name})
+            return jsonify({"choices": search_result, "artist": artist_name})
 
         artist_id = search_result[0]["id"]
 
-    created_playlist_name = createplaylist.discography_playlist(
+    created_playlist_name = discography_playlist(
         artist_name,
         artist_id,
         release_filter=release_filter,
         pandora_config={"auth_token": request.headers.get("X-PandoraAuthToken")})
     return jsonify({"created": created_playlist_name})
-       
+
+@app.route("/create/similar", methods=["POST"])
+def create_similar_artists_playlist():
+    if not request.headers.get("X-PandoraAuthToken"):
+        return jsonify({}), 401
+
+    release_filter = musicbrainz.Filter.create(
+        include_eps=request.form.get("eps", "False") == "True",
+        include_singles=request.form.get("singles", "False") == "True",
+        include_compilations=request.form.get("compilations", "False") == "True",
+        include_remixes=request.form.get("remixes", "False") == "True",
+        include_live=request.form.get("live", "False") == "True",
+        include_soundtracks=request.form.get("soundtracks", "False") == "True")
+
+    src_artist_name = request.form["artistName"]
+    src_artist_id = request.form.get("artistId")
+    similar_artist_ids = json.loads(request.form.get("similarArtistIds", "[]"))
+
+    if not src_artist_id or not similar_artist_ids:
+        pandora_client = pandora.Pandora.connect(auth_token=request.headers.get("X-PandoraAuthToken"))
+
+        src_artist_choices = _get_similar_artists(pandora_client, src_artist_name)
+        for src_artist_choice in src_artist_choices:
+            for similar_artist in src_artist_choice["similar"]:
+                similar_artist["choices"] = _search_musicbrainz(similar_artist["name"])
+
+        if len(src_artist_choices) != 1 or \
+                any(len(similar_artist["choices"]) != 1 for similar_artist in src_artist_choices[0]["similar"]):
+            return jsonify({"choices": src_artist_choices, "artist": src_artist_name})
+
+        src_artist_id = src_artist_choices[0]["id"]
+        similar_artist_ids = [similar["id"] for similar in src_artist_choices[0]["similar"].values()]
+
+    created_playlist_name = similar_artists_playlist(
+        src_artist_name,
+        src_artist_id,
+        similar_artist_ids,
+        release_filter=release_filter,
+        pandora_config={"auth_token": request.headers.get("X-PandoraAuthToken")})
+
+    return jsonify({"created": created_playlist_name})  
 
 @app.route("/display", methods=["GET", "POST"])
 def display_playlist():
